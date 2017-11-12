896ca7e7aba89d769df3036caf7aa8359bc46ac2
jasonpeixoto <nanohexcorp@gmail.com>
2017-11-11 20:37:19 -0500

    Update iot_wifi.py

--- src/iot_wifi.py
+++ src/iot_wifi.py
@@ -1,6 +1,7 @@
-# IoT Esp8266 by Jason Peixoto 
-# IOT EcoSystem (c) 
-# www.iotecosystem.com
+# IoT Esp8266 Using MicroPython http://micropython.org/
+# IOT EcoSystem (c) is copyrighted by Jason Peixoto 
+# http://www.iotecosystem.com
+# Jason Peixoto 
 # GNU GENERAL PUBLIC LICENSE 3
 import network
 import utime
