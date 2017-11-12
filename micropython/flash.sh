a3ea104d657c1ef9b7644aca4367b3d461331f4a
jasonpeixoto <nanohexcorp@gmail.com>
2017-11-11 20:12:57 -0500

    Update flash.sh

--- micropython/flash.sh
+++ micropython/flash.sh
@@ -1,3 +1,9 @@
+# Flash esp8266 by Jason Peixoto 
+# IOT EcoSystem (c) 
+# www.iotecosystem.com
+#
+# GNU GENERAL PUBLIC LICENSE 3
+#
 cd esptool
 ./esptool.py --port /dev/tty.SLAB_USBtoUART erase_flash 
 ./esptool.py --port /dev/tty.SLAB_USBtoUART --baud 460800 write_flash --flash_size=detect 0 ../esp8266-20171101-v1.9.3.bin
