7695a752949bb2f94778bbef9f6317c425a8cf4d
jasonpeixoto <nanohexcorp@gmail.com>
2017-11-11 20:41:13 -0500

    Update meetup.1.0.11.py

--- src/meetup.1.0.11.py
+++ src/meetup.1.0.11.py
@@ -1,3 +1,8 @@
+# IoT Esp8266 Using MicroPython http://micropython.org/
+# IOT EcoSystem (c) is copyrighted by Jason Peixoto 
+# http://www.iotecosystem.com
+# Jason Peixoto 
+# GNU GENERAL PUBLIC LICENSE 3
 import iot_config
 import iot_wifi
 import iot_mqtt
