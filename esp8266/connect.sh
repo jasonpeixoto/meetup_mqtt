535a306f1b3385efbb5ced0605ebba57b0b05fd3
jasonpeixoto <nanohexcorp@gmail.com>
2017-11-11 20:17:45 -0500

    Update connect.sh

--- esp8266/connect.sh
+++ esp8266/connect.sh
@@ -1,1 +1,8 @@
+# Connect esp8266 by Jason Peixoto 
+# IOT EcoSystem (c) 
+# www.iotecosystem.com
+#
+# change /dev/tty.SLAB_USBtoUART to your own serial port
+#
+# GNU GENERAL PUBLIC LICENSE 3
 rshell --buffer-size=30 -p /dev/tty.SLAB_USBtoUART 
