f6e379f7f7f482b36f6673efc1a4c4352143bbd6
Jason Peixoto <jason@iotecosystem.com>
2017-11-11 22:09:44 -0500

    remove old code

--- src/meetup.1.0.00.py
+++
@@ -1,6 +0,0 @@
-# IoT Esp8266 Using MicroPython http://micropython.org/
-# IOT EcoSystem (c) is copyrighted by Jason Peixoto 
-# http://www.iotecosystem.com
-# Jason Peixoto 
-# GNU GENERAL PUBLIC LICENSE 3
-print("Hello World")
--- src/meetup.1.0.04.py
+++
@@ -1,9 +0,0 @@
-# IoT Esp8266 Using MicroPython http://micropython.org/
-# IOT EcoSystem (c) is copyrighted by Jason Peixoto 
-# http://www.iotecosystem.com
-# Jason Peixoto 
-# GNU GENERAL PUBLIC LICENSE 3
-import iot_config
-import iot_wifi
-
-iot_wifi.connect_wifi(iot_config.wifi_ssid, iot_config.wifi_password)
